b59cdb36220bd8bc56ca571c5a37d076f325710e
Philippe Gerum <rpm@xenomai.org>
2020-12-31 17:32:55 +0100

    cobalt/syscall: dovetail: do not expose clock frequency to user
    We don't need this with Dovetail, there is no timestamp arithmetics to
    perform in userland whatsoever: we get timestamps from the regular
    vDSO-based clock_gettime() call.
    
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- include/cobalt/kernel/dovetail/pipeline/pipeline.h
+++ include/cobalt/kernel/dovetail/pipeline/pipeline.h
@@ -7,6 +7,7 @@
 
 #include <linux/irq_pipeline.h>
 #include <cobalt/kernel/assert.h>
+#include <asm/xenomai/features.h>
 
 typedef unsigned long spl_t;
 
@@ -100,6 +101,11 @@ static inline void pipeline_free_timer_ipi(void)
 static inline void pipeline_prepare_panic(void)
 {
 	/* N/A */
+}
+
+static inline void pipeline_collect_features(struct cobalt_featinfo *f)
+{
+	f->clock_freq = 0;	/* N/A */
 }
 
 #endif /* !_COBALT_KERNEL_DOVETAIL_PIPELINE_H */
