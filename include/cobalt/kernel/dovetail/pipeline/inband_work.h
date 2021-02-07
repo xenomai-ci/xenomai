abcbf5d2e6774dec4db5cf437e6bd8dcf4f821fd
Philippe Gerum <rpm@xenomai.org>
2021-02-07 17:04:21 +0100

    cobalt/inband_work: dovetail: provide for deferred in-band calls
    Dovetail enables irq_work for deferring the execution of a routine
    until the in-band stage is active again on the current CPU, we don't
    need to provide for any additional mechanism. Therefore we just need
    to wrap the pipeline interface calls to the irq_work API.
    
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

---
+++ include/cobalt/kernel/dovetail/pipeline/inband_work.h
@@ -0,0 +1,32 @@
+/*
+ * SPDX-License-Identifier: GPL-2.0
+ *
+ * Copyright (C) 2020 Philippe Gerum  <rpm@xenomai.org>
+ */
+
+#ifndef _COBALT_KERNEL_DOVETAIL_INBAND_WORK_H
+#define _COBALT_KERNEL_DOVETAIL_INBAND_WORK_H
+
+#include <linux/irq_work.h>
+
+/*
+ * This field must be named inband_work and appear first in the
+ * container work struct.
+ */
+struct pipeline_inband_work {
+	struct irq_work work;
+};
+
+#define PIPELINE_INBAND_WORK_INITIALIZER(__work, __handler)		\
+	{								\
+		.work = {						\
+			.func = (void (*)(struct irq_work *))		\
+			__handler,					\
+			.flags = ATOMIC_INIT(0),			\
+		},							\
+	}
+
+#define pipeline_post_inband_work(__work)				\
+			irq_work_queue(&(__work)->inband_work.work)
+
+#endif /* !_COBALT_KERNEL_DOVETAIL_INBAND_WORK_H */
