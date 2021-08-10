xenomai-ci/xenomai

commit 25a53f5cd35242a1a4f616ca3c6eefeb4c7ee673

rtnet: udp: Remove duplicate copy_from/to_user
The RTNET sendmsg/recvmsg protocol handlers used to call
copy_to/from_user on the struct user_msghdr argument. The syscall entry
code already does this copy, so calling again the copy_to/from_user in
handlers triggers SPECTRE mitigation protection on ARM. This patch
removes the calls in the handlers.

This patch has not been tested

Signed-off-by: François LEGAL <devel@thom.fr.eu.org>
[Jan: massage commit log]
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

kernel/drivers/net/stack/ipv4/udp/udp.c
@@ -386,7 +386,7 @@ int rt_udp_ioctl(struct rtdm_fd *fd, unsigned int request, void __user *arg)
 /***
  *  rt_udp_recvmsg
  */
-ssize_t rt_udp_recvmsg(struct rtdm_fd *fd, struct user_msghdr *u_msg,
+ssize_t rt_udp_recvmsg(struct rtdm_fd *fd, struct user_msghdr *msg,
 		       int msg_flags)
 {
 	struct rtsocket *sock = rtdm_fd_to_private(fd);
@@ -400,13 +400,8 @@ ssize_t rt_udp_recvmsg(struct rtdm_fd *fd, struct user_msghdr *u_msg,
 	struct sockaddr_in sin;
 	nanosecs_rel_t timeout = sock->timeout;
 	int ret, flags;
-	struct user_msghdr _msg, *msg;
 	socklen_t namelen;
 	struct iovec iov_fast[RTDM_IOV_FASTMAX], *iov;
-
-	msg = rtnet_get_arg(fd, &_msg, u_msg, sizeof(_msg));
-	if (IS_ERR(msg))
-		return PTR_ERR(msg);
 
 	if (msg->msg_iovlen < 0)
 		return -EINVAL;
@@ -451,7 +446,7 @@ ssize_t rt_udp_recvmsg(struct rtdm_fd *fd, struct user_msghdr *u_msg,
 			goto fail;
 
 		namelen = sizeof(sin);
-		ret = rtnet_put_arg(fd, &u_msg->msg_namelen, &namelen,
+		ret = rtnet_put_arg(fd, &msg->msg_namelen, &namelen,
 				    sizeof(namelen));
 		if (ret)
 			goto fail;
@@ -495,7 +490,7 @@ ssize_t rt_udp_recvmsg(struct rtdm_fd *fd, struct user_msghdr *u_msg,
 		flags |= MSG_TRUNC;
 
 	if (flags != msg->msg_flags) {
-		ret = rtnet_put_arg(fd, &u_msg->msg_flags, &flags,
+		ret = rtnet_put_arg(fd, &msg->msg_flags, &flags,
 				    sizeof(flags));
 		if (ret)
 			goto fail;
@@ -589,7 +584,6 @@ ssize_t rt_udp_sendmsg(struct rtdm_fd *fd, const struct user_msghdr *msg,
 	u16 dport;
 	int err;
 	rtdm_lockctx_t context;
-	struct user_msghdr _msg;
 	struct iovec iov_fast[RTDM_IOV_FASTMAX], *iov;
 
 	if (msg_flags & MSG_OOB) /* Mirror BSD error message compatibility */
@@ -597,10 +591,6 @@ ssize_t rt_udp_sendmsg(struct rtdm_fd *fd, const struct user_msghdr *msg,
 
 	if (msg_flags & ~(MSG_DONTROUTE | MSG_DONTWAIT))
 		return -EINVAL;
-
-	msg = rtnet_get_arg(fd, &_msg, msg, sizeof(*msg));
-	if (IS_ERR(msg))
-		return PTR_ERR(msg);
 
 	if (msg->msg_iovlen < 0)
 		return -EINVAL;

commit 1ee4f1a2a090dc9c50f851635e95288101d4c0cb

drivers/net: udp, packet: Fix returning of msg_namelen and msg_flags from recvmsg
The msg struct passed to the recvmsg handler for udp and packet sockets
is not located in userspace. Therefore, we must not use rtnet_put_arg to
update its content, it may fail on some archs, namely arm and arm64.
Just assign, the syscall entry function will take care of copying it
back.

Fixes: 25a53f5cd352 ("rtnet: udp: Remove duplicate copy_from/to_user")
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

kernel/drivers/net/stack/ipv4/udp/udp.c
@@ -446,10 +446,7 @@ ssize_t rt_udp_recvmsg(struct rtdm_fd *fd, struct user_msghdr *msg,
 			goto fail;
 
 		namelen = sizeof(sin);
-		ret = rtnet_put_arg(fd, &msg->msg_namelen, &namelen,
-				    sizeof(namelen));
-		if (ret)
-			goto fail;
+		msg->msg_namelen = namelen;
 	}
 
 	data_len = ntohs(uh->len) - sizeof(struct udphdr);
@@ -489,12 +486,7 @@ ssize_t rt_udp_recvmsg(struct rtdm_fd *fd, struct user_msghdr *msg,
 	if (data_len > 0)
 		flags |= MSG_TRUNC;
 
-	if (flags != msg->msg_flags) {
-		ret = rtnet_put_arg(fd, &msg->msg_flags, &flags,
-				    sizeof(flags));
-		if (ret)
-			goto fail;
-	}
+	msg->msg_flags = flags;
 out:
 	if ((msg_flags & MSG_PEEK) == 0)
 		kfree_rtskb(first_skb);

kernel/drivers/net/stack/packet/af_packet.c
@@ -302,7 +302,7 @@ static ssize_t rt_packet_recvmsg(struct rtdm_fd *fd, struct user_msghdr *msg,
 	size_t copy_len;
 	struct rtskb *rtskb;
 	struct sockaddr_ll sll;
-	int ret, flags;
+	int ret;
 	nanosecs_rel_t timeout = sock->timeout;
 	socklen_t namelen;
 	struct iovec iov_fast[RTDM_IOV_FASTMAX], *iov;
@@ -355,10 +355,7 @@ static ssize_t rt_packet_recvmsg(struct rtdm_fd *fd, struct user_msghdr *msg,
 			goto fail;
 
 		namelen = sizeof(sll);
-		ret = rtnet_put_arg(fd, &msg->msg_namelen, &namelen,
-				    sizeof(namelen));
-		if (ret)
-			goto fail;
+		msg->msg_namelen = namelen;
 	}
 
 	/* Include the header in raw delivery */
@@ -375,11 +372,7 @@ static ssize_t rt_packet_recvmsg(struct rtdm_fd *fd, struct user_msghdr *msg,
 
 	if (copy_len > len) {
 		copy_len = len;
-		flags = msg->msg_flags | MSG_TRUNC;
-		ret = rtnet_put_arg(fd, &msg->msg_flags, &flags,
-				    sizeof(flags));
-		if (ret)
-			goto fail;
+		msg->msg_flags |= MSG_TRUNC;
 	}
 
 	copy_len = rtnet_write_to_iov(fd, iov, msg->msg_iovlen, rtskb->data,