c8aae17c4dc9a5b5730d70c54cbf0ce0e7ed2103
Jan Kiszka <jan.kiszka@siemens.com>
2022-02-18 13:49:58 +0100

    lib: Re-add "static" to pthread_mutexattr_setrobust fallback stub
    Lost in 3dfa855b6dd0.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- include/boilerplate/libc.h
+++ include/boilerplate/libc.h
@@ -247,8 +247,9 @@ __weak int shm_unlink(const char *name)
 #ifdef HAVE_PTHREAD_MUTEXATTR_SETROBUST_NP
 #define pthread_mutexattr_setrobust	pthread_mutexattr_setrobust_np
 #else
-inline int pthread_mutexattr_setrobust(pthread_mutexattr_t *attr,
-				       int robustness)
+static inline
+int pthread_mutexattr_setrobust(pthread_mutexattr_t *attr,
+				int robustness)
 {
 	return ENOSYS;
 }
