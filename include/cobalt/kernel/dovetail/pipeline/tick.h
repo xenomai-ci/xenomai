82badafc0258dc8c2f78b68612b1dc3ea6ac4bc4
Jan Kiszka <jan.kiszka@siemens.com>
2023-09-03 10:21:13 +0200

    include/cobalt: Fix inclusion guard names
    Copy&pasted from their former I-pipe sibling.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- include/cobalt/kernel/dovetail/pipeline/tick.h
+++ include/cobalt/kernel/dovetail/pipeline/tick.h
@@ -2,8 +2,8 @@
  * SPDX-License-Identifier: GPL-2.0
  */
 
-#ifndef _COBALT_KERNEL_IPIPE_TICK_H
-#define _COBALT_KERNEL_IPIPE_TICK_H
+#ifndef _COBALT_KERNEL_DOVETAIL_TICK_H
+#define _COBALT_KERNEL_DOVETAIL_TICK_H
 
 int pipeline_install_tick_proxy(void);
 
@@ -13,4 +13,4 @@ struct xnsched;
 
 bool pipeline_must_force_program_tick(struct xnsched *sched);
 
-#endif /* !_COBALT_KERNEL_IPIPE_TICK_H */
+#endif /* !_COBALT_KERNEL_DOVETAIL_TICK_H */
