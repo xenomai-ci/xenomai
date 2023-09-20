48f22fcfbdcc07e34b49991f213d2fc7c9302013
Aaron Marcher <aaron@sigma-star.at>
2023-09-20 15:52:51 +0200

    testsuite: Merge psostests in a single smokey case
    Cleans up includes
    
    Signed-off-by: Aaron Marcher <aaron@sigma-star.at>
    [Jan: set affinity via smokey_run_extprog args]
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- testsuite/smokey/psostests/psostests.c
+++ testsuite/smokey/psostests/psostests.c
@@ -1,68 +1,70 @@
 // SPDX-License-Identifier: GPL-2.0
 #include <error.h>
-#include <libgen.h>
-#include <smokey/smokey.h>
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
+#include <smokey/smokey.h>
 
-#define TEST(name)								   \
-	smokey_test_plugin(name, SMOKEY_NOARGS, "Run external test");		   \
-	static int run_##name(struct smokey_test *t, int argc, char *const argv[]) \
-	{									   \
-		return __run_extprog(t, argc, argv);				   \
-	}
-
-static int __run_extprog(struct smokey_test *t, int argc, char *const argv[])
+static int run_psostests(struct smokey_test *t, int argc, char *const argv[])
 {
-	int ret;
-	char *tst_path;
+	int ret = 0;
 
-	ret = asprintf(&tst_path, "%s/%s --cpu-affinity=0", XENO_TEST_DIR, t->name);
-	if (ret == -1)
-		return -ENOMEM;
+	char *tests[] = {
+		"psostests_mq1",
+		"psostests_mq2",
+		"psostests_mq3",
+		"psostests_pt1",
+		// 0"000.679| BUG in __traceobj_assert_failed(): [rn1] trace assertion failed:
+		// testsuite/psostests/rn-1.c:46
+		// => "ret == 0"
+		//"psostests_rn1"
+		"psostests_sem1",
+		"psostests_sem2",
+		"psostests_task1",
+		// 0"014.452| BUG in __traceobj_assert_failed(): [FGND] trace assertion failed:
+		//		../../../../../../../../../workspace/sources/xenomai/testsuite/smokey/psostests/task-2.c:56 => "ret == 0"
+		//"psostests_task2",
+		"psostests_task3",
+		"psostests_task4",
+		"psostests_task5",
+		// 0"011.370| BUG in __traceobj_assert_failed(): [FGND] trace assertion failed:
+		//		testsuite/psostests/task-6.c:62
+		// => "ret == 0 && oldprio == myprio"
+		//"psostests_task6",
+		"psostests_task7",
+		// Runs forever (100% CPU)
+		//"psostests_task8"
+		// 0"002.198| WARNING: [main] lack of resources for core thread, EBUSY
+		// 0"003.421| BUG in __traceobj_assert_failed(): [root] trace assertion failed:
+		//	   ../../../../../../../../../workspace/sources/xenomai/testsuite/smokey/psostests/task-9.c:29 => "ret == 0"
+		//"psostests_task9",
+		"psostests_tm1",
+		"psostests_tm2",
+		"psostests_tm3",
+		"psostests_tm4",
+		"psostests_tm5",
+		"psostests_tm6",
+		"psostests_tm7"
+	};
 
-	ret = system(tst_path);
-	free(tst_path);
+	for (size_t t = 0; t < sizeof(tests) / sizeof(tests[0]); t++)
+	{
+		int fails = 0;
+
+		ret = smokey_run_extprog(XENO_TEST_DIR, tests[t],
+					 "--cpu-affinity=0");
+		if (ret) {
+			fails++;
+			if (smokey_keep_going)
+				continue;
+			if (smokey_verbose_mode)
+				error(1, -ret, "test %s failed", tests[t]);
+			return 1;
+		}
+		smokey_note("%s OK", tests[t]);
+	}
 
 	return ret;
 }
+smokey_test_plugin(psostests, SMOKEY_NOARGS, "Run external psostests");
 
-TEST(psostests)
-TEST(psostests_mq1)
-TEST(psostests_mq2)
-TEST(psostests_mq3)
-TEST(psostests_pt1)
-// 0"000.679| BUG in __traceobj_assert_failed(): [rn1] trace assertion failed:
-//            testsuite/psostests/rn-1.c:46
-// => "ret == 0"
-//TEST(psostests_rn1)
-TEST(psostests_sem1)
-TEST(psostests_sem2)
-TEST(psostests_task1)
-// 0"014.452| BUG in __traceobj_assert_failed(): [FGND] trace assertion failed:
-//            ../../../../../../../../../workspace/sources/xenomai/testsuite/smokey/psostests/task-2.c:56 => "ret == 0"
-//TEST(psostests_task2)
-TEST(psostests_task3)
-TEST(psostests_task4)
-TEST(psostests_task5)
-// 0"011.370| BUG in __traceobj_assert_failed(): [FGND] trace assertion failed:
-//            testsuite/psostests/task-6.c:62
-// => "ret == 0 && oldprio == myprio"
-// TEST(psostests_task6)
-TEST(psostests_task7)
-// runs forever (100% CPU)
-//TEST(psostests_task8)
-// 0"002.198| WARNING: [main] lack of resources for core thread, EBUSY
-// 0"003.421| BUG in __traceobj_assert_failed(): [root] trace assertion failed:
-//           ../../../../../../../../../workspace/sources/xenomai/testsuite/smokey/psostests/task-9.c:29 => "ret == 0"
-//TEST(psostests_task9)
-TEST(psostests_tm1)
-TEST(psostests_tm2)
-TEST(psostests_tm3)
-TEST(psostests_tm4)
-TEST(psostests_tm5)
-TEST(psostests_tm6)
-TEST(psostests_tm7)
