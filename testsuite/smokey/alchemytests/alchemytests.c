9b08f9b87fe2ee27ab738d17ae47fc766899fc6e
Aaron Marcher <aaron@sigma-star.at>
2023-09-20 15:52:50 +0200

    testsuite: Merge alchemytests in single smokey case
    Signed-off-by: Aaron Marcher <aaron@sigma-star.at>
    [Jan: set affinity via smokey_run_extprog args]
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- testsuite/smokey/alchemytests/alchemytests.c
+++ testsuite/smokey/alchemytests/alchemytests.c
@@ -1,55 +1,57 @@
 // SPDX-License-Identifier: GPL-2.0
 #include <error.h>
-#include <libgen.h>
-#include <smokey/smokey.h>
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
+#include <smokey/smokey.h>
 
-#define TEST(name)								   \
-	smokey_test_plugin(name, SMOKEY_NOARGS, "Run external test");		   \
-	static int run_##name(struct smokey_test *t, int argc, char *const argv[]) \
-	{									   \
-		return __run_extprog(t, argc, argv);				   \
-	}
-
-static int __run_extprog(struct smokey_test *t, int argc, char *const argv[])
+static int run_alchemytests(struct smokey_test *t, int argc, char *const argv[])
 {
-	int ret;
-	char *tst_path;
+	int ret = 0;
 
-	ret = asprintf(&tst_path, "%s/%s --cpu-affinity=0", XENO_TEST_DIR, t->name);
-	if (ret == -1)
-		return -ENOMEM;
+	char *tests[] = {
+		"alchemytests_alarm1",
+		"alchemytests_buffer1",
+		"alchemytests_event1",
+		"alchemytests_heap1",
+		"alchemytests_heap2",
+		"alchemytests_mq1",
+		"alchemytests_mq2",
+		"alchemytests_mq3",
+		"alchemytests_mutex1",
+		"alchemytests_pipe1",
+		"alchemytests_sem1",
+		"alchemytests_sem2",
+		"alchemytests_task1",
+		"alchemytests_task2",
+		"alchemytests_task3",
+		"alchemytests_task4",
+		"alchemytests_task5",
+		"alchemytests_task6",
+		"alchemytests_task7",
+		"alchemytests_task8",
+		"alchemytests_task9",
+		"alchemytests_task10"
+	};
 
-	ret = system(tst_path);
-	free(tst_path);
+	for (size_t t = 0; t < sizeof(tests) / sizeof(tests[0]); t++)
+	{
+		int fails = 0;
+
+		ret = smokey_run_extprog(XENO_TEST_DIR, tests[t],
+					 "--cpu-affinity=0");
+		if (ret) {
+			fails++;
+			if (smokey_keep_going)
+				continue;
+			if (smokey_verbose_mode)
+				error(1, -ret, "test %s failed", tests[t]);
+			return 1;
+		}
+		smokey_note("%s OK", tests[t]);
+	}
 
 	return ret;
 }
+smokey_test_plugin(alchemytests, SMOKEY_NOARGS, "Run external alchemytests");
 
-TEST(alchemytests)
-TEST(alchemytests_alarm1)
-TEST(alchemytests_buffer1)
-TEST(alchemytests_event1)
-TEST(alchemytests_heap1)
-TEST(alchemytests_heap2)
-TEST(alchemytests_mq1)
-TEST(alchemytests_mq2)
-TEST(alchemytests_mq3)
-TEST(alchemytests_mutex1)
-TEST(alchemytests_pipe1)
-TEST(alchemytests_sem1)
-TEST(alchemytests_sem2)
-TEST(alchemytests_task1)
-TEST(alchemytests_task2)
-TEST(alchemytests_task3)
-TEST(alchemytests_task4)
-TEST(alchemytests_task5)
-TEST(alchemytests_task6)
-TEST(alchemytests_task7)
-TEST(alchemytests_task8)
-TEST(alchemytests_task9)
-TEST(alchemytests_task10)
