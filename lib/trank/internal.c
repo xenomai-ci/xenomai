95e3a1e4485410c23477bca0cc8b1f2e09e3a061
Florian Bezdeka <florian.bezdeka@siemens.com>
2023-12-05 21:41:49 +0900

    lib/trank: Cleanup includes
    - Add an empty line between license information and first include,
      that is what most files already did
    
    - Cleanup unused / unnecessary includes where possible
    
    Signed-off-by: Florian Bezdeka <florian.bezdeka@siemens.com>
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- lib/trank/internal.c
+++ lib/trank/internal.c
@@ -15,13 +15,13 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
  */
+
 #include <xeno_config.h>
-#include <stdio.h>
 #include <signal.h>
 #include <stdlib.h>
 #include <string.h>
-#include <memory.h>
 #include <boilerplate/signal.h>
+
 #include "cobalt/internal.h"
 #include "internal.h"
 
