82c708a9e12fd60b18f0d7e03de7d859b51f8eff
Florian Bezdeka <florian.bezdeka@siemens.com>
2024-10-12 12:26:42 +0200

    libcobalt: arm: Cleanup unused includes
    No functional change, preparation work for the asm/xenomai/syscall.h
    removal.
    
    Signed-off-by: Florian Bezdeka <florian.bezdeka@siemens.com>
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- lib/cobalt/arch/arm/features.c
+++ lib/cobalt/arch/arm/features.c
@@ -14,21 +14,12 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
  */
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/mman.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <string.h>
-#include <string.h>
-#include <unistd.h>
-#include <limits.h>
-#include <cobalt/wrappers.h>
-#include <cobalt/ticks.h>
-#include <asm/xenomai/syscall.h>
+
 #include <asm/xenomai/features.h>
 #include <asm/xenomai/uapi/fptest.h>
+#include <stdio.h>
+#include <string.h>
+
 #include "internal.h"
 
 void cobalt_arch_check_features(struct cobalt_featinfo *finfo)
